894690c9fb66df49d5b127b3eb847176b55fca07
Warren Rebello <117888816+warrenrebellosnowflake@users.noreply.github.com>
2022-11-10 12:33:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,9 @@ streamlit.text("🥑🍞 Avocade Toast")
 
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
+
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
