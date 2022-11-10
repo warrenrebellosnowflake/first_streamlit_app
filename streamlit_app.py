71d24628b17ad03bb220dfb6abc29261b1b93249
Warren Rebello <117888816+warrenrebellosnowflake@users.noreply.github.com>
2022-11-10 12:38:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,8 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+# Adding a pick list so users can select the fruit they want
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+
+# Display table on page
 streamlit.dataframe(my_fruit_list)
