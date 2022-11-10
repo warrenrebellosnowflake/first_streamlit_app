ae4564d5bd8ab6be7adfa10e4d1d358ab4ffa4f7
Warren Rebello <117888816+warrenrebellosnowflake@users.noreply.github.com>
2022-11-10 11:59:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,1 @@
-import streamlit
-
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header('Breafast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+print('Hello World')
